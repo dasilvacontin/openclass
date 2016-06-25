3c9c3dc3f1f10077b67d297f7db204b1c56fd3d8
David da Silva <dasilvacontin@gmail.com>
2016-06-25 12:25:29 +0200

    Initial commit

---
+++ noobdb.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const onDeath = require('death')
+
+onDeath(_ => { process.exit() })
+
+function saveStore(file, store) {
+  console.log(`noobdb: saving DB to ${file}...`)
+  fs.writeFileSync(file, JSON.stringify(store))
+}
+
+module.exports = function noobdb (file, defaultObj) {
+  let store = defaultObj
+
+  try {
+    console.log(`noobdb: reading DB from ${file}...`)
+    store = JSON.parse(fs.readFileSync(file))
+  } catch (e) {
+    console.log(`failed to load ${file}: ${e.message}`)
+    saveStore(file, store)
+  }
+
+  process.on('exit', _ => { saveStore(file, store) })
+  return store
+}
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const ip = require('ip')
+const port = process.env.PORT || 3000
+const noobdb = require('./noobdb.js')
+const messages = noobdb('db.json', [])
+
+const _log = console.log
+console.log = function () {
+  const d = new Date()
+  const args = Array.from(arguments)
+  const timestamp = '[' + d.toLocaleString().split(' ').slice(0, 2).join(' ') + ']'
+  _log.apply(console, [timestamp].concat(args))
+}
+
+app.use(express.static('.'))
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+  socket.emit('bootstrap', socket.id, messages)
+
+  socket.on('message:create', (value) => {
+    console.log('message:create', socket.id, value)
+    value = String(value).trim()
+    if (value.length === 0) return
+
+    const message = {
+      id: messages.length,
+      timestamp: Date.now(),
+      username: 'Anon',
+      content: value,
+      upvotes: 0
+    }
+    messages.push(message)
+
+    io.sockets.emit('message:create', message)
+  })
+
+  socket.on('message:upvote', (messageId) => {
+    console.log('message:upvote', socket.id, messageId)
+    console.log(socket.conn.remoteAddress)
+    const message = messages[messageId]
+    if (!message) return console.log('upvoted unexisting message')
+
+    message.upvotes++
+    io.sockets.emit('message:upvote', messageId, socket.id)
+  })
+})
+
+http.listen(3000, function () {
+  console.log(`listening on ${ip.address()}:${port}`)
+})
